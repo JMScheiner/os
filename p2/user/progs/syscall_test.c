19381b87f820d8e52c155cccd6e2efdabc382c4f
Justin M. Scheiner <jscheine@unix11.andrew.cmu.edu>
2010-09-16 23:42:15 -0400

    Fixed an important typo in exec.S

--- p2/user/progs/syscall_test.c
+++ p2/user/progs/syscall_test.c
@@ -11,6 +11,8 @@ int main(int argc, const char *argv[])
 	if((pid = fork()) == 0)
 	{
 		printf("Hello from child!\n");
+		sleep(10000);
+		printf("10000 ticks have gone by!\n");
 		return 15;
 	}
 	else
@@ -18,8 +20,12 @@ int main(int argc, const char *argv[])
 		printf("Hello from parent!\n");
 		wait(&status);
 		printf("Child (pid = %d) exited with %d.  Should be 15.\n", pid, status);
-
 	}
+
+	//Run again!!!
+	char* argvec[] = {"syscall_test", 0};
+	exec("syscall_test", argvec);
+	
 	return 8;
 }
 
