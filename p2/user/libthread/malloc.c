3e0db25fe8f17f7b116c3a4912259b0cabedca50
Justin M. Scheiner <jscheine@unix35.andrew.cmu.edu>
2010-09-24 13:15:55 -0400

    Basic implementation of thread safe malloc.
    As simple as I think the problem is at the moment.

--- p2/user/libthread/malloc.c
+++ p2/user/libthread/malloc.c
@@ -3,28 +3,62 @@
  * It is up to you to rewrite them
  * to make them thread safe.
  *
+ * Justin - All I did here was wrap the nonthreadsafe functions
+ * 	in a "heap lock." Further thought may be required.
+ *
  */
 
 #include <stdlib.h>
 #include <types.h>
 #include <stddef.h>
+#include <mutex.h>
+
+mutex_t heap_lock;
 
 void *malloc(size_t __size)
 {
-  return NULL;
+	void* ret;
+	
+	mutex_lock(&heap_lock);
+	
+	ret = _malloc(__size);	
+	
+	mutex_unlock(&heap_lock);
+	
+	return ret;
 }
 
 void *calloc(size_t __nelt, size_t __eltsize)
 {
-  return NULL;
+	void* ret;
+	
+	mutex_lock(&heap_lock);
+	
+	ret = _calloc(__nelt, __eltsize);	
+	
+	mutex_unlock(&heap_lock);
+
+	return ret;
 }
 
 void *realloc(void *__buf, size_t __new_size)
 {
-  return NULL;
+	void* ret;
+	
+	mutex_lock(&heap_lock);
+	
+	ret = _realloc(__buf, __new_size);	
+	
+	mutex_unlock(&heap_lock);
+	
+	return ret;
 }
 
 void free(void *__buf)
 {
-  return;
+	mutex_lock(&heap_lock);
+	
+	free(__buf);	
+	
+	mutex_unlock(&heap_lock);
 }
