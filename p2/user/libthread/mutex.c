9c3b4d871b064a5b5830dff2e8b6a7676288246e
Justin M. Scheiner <jscheine@ghc72.ghc.andrew.cmu.edu>
2010-09-24 17:09:21 -0400

    Push for debugging.
    asdf

--- p2/user/libthread/mutex.c
+++ p2/user/libthread/mutex.c
@@ -13,6 +13,9 @@
 #include <stddef.h>
 #include <simics.h>
 #include <stdio.h>
+#include <simics.h>
+
+static int mutex_id = 0;
 
 /** 
 * @brief Initialize a mutex for locking.
@@ -23,14 +26,10 @@
 */
 int mutex_init(mutex_t *mp)
 {
-	if(!mp)
-		return -1;
-
-	mp->last = NULL;
-	mp->running = NULL;
-	mp->next = NULL;
-	mp->held = FALSE;
-	mp->tid = 0;
+	mutex_id++;
+	mp->ticket = 0;
+	mp->now_serving = 0;
+	mp->active_tid = 0;
 	mp->initialized = TRUE;
 	return 0;
 }
@@ -46,14 +45,6 @@ int mutex_init(mutex_t *mp)
 */
 int mutex_destroy(mutex_t *mp)
 {
-	if(!mp)
-		return -1;
-	
-	//FIXME mutex_destroy needs to atomically 
-	//	deactivate the mutex, so we can block 
-	//	invocations of lock_mutex, and return -1 if someone
-	//	holds the mutex illegally.
-	
 	mp->initialized = FALSE;
 	return 0;
 }
@@ -75,158 +66,26 @@ int mutex_try_lock(mutex_t *mp)
 }
 
 
-/** 
-* @brief Locks a mutex.
-* 
-* @param mp The mutex to lock.
-*
-* 	This is complicated, but everything is serialized by atomically
-* 		swapping yourself into the end of the list.
-*	
-*	Each list element contains a basic lock which it contends for
-*		with the next thread that will call mutex_lock.
-*		
-*		- Threads "try_lock" the lock in the parent.
-*			
-*			- If they succeed in acquiring their parents lock, they can safely
-*				tell the parent who they are, to prepare for descheduling.
-*
-*				- The parent is able to cancel deschedule by setting cancel_deschedule in the child,
-*					so there isn't a race condition between deschedule and make_runnable.
-*			
-*			- If they fail to acquire the lock, they busy wait for him to finish
-*				on the value "held" in the mutex.
-*			
-*		- When parents acquire the lock, they set mp->next to either NULL or their child.
-*			
-*			- This is so in mutex_unlock we can decide whether to take down the "held" flag 
-*				and let children who failed to get the lock go, or actively "make_runnable" 
-*				the next person waiting for the mutex.
-*		
-*		- Every thread is blocking on mutex->held, busy waiting on its parent, or is running in the 
-*			critical section.
-* 
-* @return 0 on success, 
-* 	-1 on failure.
-*/
 int mutex_lock( mutex_t *mp )
 {
-	if(!mp)
-		return -1;
+	int tid = thr_getid();
+	int ticket = 1;
+	atomic_xadd(&ticket, &mp->ticket);
 	
-	if(!mp->initialized)
-		return -2;
-	
-	mutex_node me;
-	mutex_node* swap = &me;
-	
-	//Initialize myself. 
-	me.tid = thr_getid();
-	me.next = NULL;
-	
-	//Can't lock a mutex we already own.
-	if(mp->tid == me.tid)
-		return -1;
-
-	tts_init(&me.access);
-	
-	//Swap yourself into the end of the mutex's list.
-	//  This is the line that guarantees bounded waiting.
-	atomic_xchg((int*)&swap, (int*)&mp->last);
-	
-	if(swap)
+	while(ticket != mp->now_serving)
 	{
-		//Protect against accessing something that is no longer on the stack:
-		int accessible = tts_try_lock(&swap->access);
-		
-		if(accessible == 0)
-		{
-			//We are free to modify the guy before us.
-			swap->next = &me;
-			me.cancel_deschedule = FALSE;
-			
-			//Open the flood gates - this works because 
-			//	(1), (2), and (3) are valid in any order.
-			tts_unlock(&swap->access);
-			deschedule((int*)&me.cancel_deschedule); /* (1) */
-		}
-		else
-		{
-			//We tried to lock but "swap" was past (4)
-			//We are next to run.
-			
-			// What if the other guy runs ALL the way through
-			// 	before we get to this point? 
-			// 		- Everyone after us is blocking above, 
-			// 		- mp->running is still 0. (who would have changed it?)
-			while(mp->held == TRUE)
-				yield(mp->tid);
-		}
+		yield(mp->active_tid);
 	}
-	
-	/************* Lock Acquired **************/
 
-	mp->held = TRUE;
-	mp->tid = me.tid;
-
-	//This, "running" will only serve as an identifier to insure that 
-	// someone has modified the list by the time we finish. It will never
-	// actually be accessed.
-	mp->running = &me;
-	
-	//Protect against accessing "me" after this point:
-	
-	tts_lock(&me.access); /* (4) */
-	//FIXME ^^ This lock isn't magic. It NEEDS to be somewhere
-	//	safe and the stack _isn't_ safe.  I could see the call
-	//	to tts_try_lock above succeeding errantly, which would
-	//		be a disaster!!!!
-	
-	mp->next = me.next;
+	//We have the mutex.
+	mp->active_tid = tid;
 	return 0;
 }
 
-/** 
-* @brief Unlock the mutex.
-*
-* Following on the description of mutex_lock, after its critical section,
-* 	a thread is responsible for:
-* 		- Releasing a busy waiting thread they don't know about, or
-* 		- make_runnable'ing a thread they do know about. "...if(next)..."
-*
-*	I would like to cmpxchg NULL into "mp->last" if possible, but this isn't
-*		necessary for unlock to be correct.
-* 
-* @param mp  The mutex to unlock.
-* 
-* @return 0 on success.
-* 	-1 on failure.
-*/
 int mutex_unlock( mutex_t *mp )
 {
-	if(!mp)
-		return -1;
-
-	if(!mp->initialized)
-		return -1;
-
-	mutex_node* next = mp->next;
-	if(next)
-	{
-		//Someone is trying to run. Let him go.
-		next->cancel_deschedule = TRUE; /* (2) */
-		make_runnable(next->tid);    /* (3) */
-	}
-	else
-	{
-		//Let a thread that wasn't able to tell this thread
-		// about itself go.
-		mp->held = FALSE;
-	}
-	
-	//TODO This would be nice, but simics is complaining about cmpxchg.
-	//next = NULL;
-	//atomic_cmpxchg((int*)&next, (int*)&mp->last, (int)mp->running);
+	int now_serving = 1;
+	atomic_xadd(&now_serving, &mp->now_serving);
 	return 0;
 }
 
@@ -245,10 +104,12 @@ int tts_try_lock(tts_lock_t* lock)
 	
 	if(flag == 1)
 	{
+		//We got the lock.
 		return 0;
 	}
 	else
 	{
+		//We failed to get the lock.
 		return -1;
 	}
 }
