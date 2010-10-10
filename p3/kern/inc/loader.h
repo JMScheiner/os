2f3226b08607962ce5fa92327fd782e480da6171
Timothy James Wilson <tjwilson@ghc20.ghc.andrew.cmu.edu>
2010-10-10 16:41:23 -0400

    make load_new_task visible

--- p3/kern/inc/loader.h
+++ p3/kern/inc/loader.h
@@ -18,4 +18,6 @@ int getbytes( const char *filename, int offset, int size, char *buf );
  * Declare your loader prototypes here.
  */
 
+int load_new_task(const char *file);
+
 #endif /* _LOADER_H */
