b9dd4a43ca63b746f8e8d1f9d26f03f4070906b2
Timothy James Wilson <tjwilson@unix34.andrew.cmu.edu>
2010-10-29 16:36:06 -0400

    debug files

---
+++ p3/kern/debug.c
@@ -0,0 +1,56 @@
+
+#include <debug.h>
+#include <simics.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define DEBUG_BUF_SIZE 256
+
+#define DEBUG_PRINT
+#define DEBUG_BREAK
+
+char *debug_print_strings[] = {"loader", "mm", "kvm", "page", "readline", NULL};
+char *debug_break_strings[] = {"page", NULL};
+
+#ifdef DEBUG_PRINT
+void debug_print(const char *type, const char *fmt, ...) {
+	int i;
+	for (i = 0; debug_print_strings[i] != NULL; i++) {
+		if (strcmp(debug_print_strings[i], type) == 0) {
+			char str[DEBUG_BUF_SIZE];
+			snprintf(str, DEBUG_BUF_SIZE - 1, "%s: ", type);
+			int end = strlen(str);
+			va_list ap;
+			va_start(ap, fmt);
+			vsnprintf(str + end, DEBUG_BUF_SIZE - 1 - end, fmt, ap);
+			va_end(ap);
+
+			sim_puts(str);
+			return;
+		}
+	}
+}
+#else
+void debug_print(const char *type, const char *fmt, ...) {
+	return;
+}
+#endif
+
+#ifdef DEBUG_BREAK
+void debug_break(const char *type) {
+	int i;
+	for (i = 0; debug_break_strings[i] != NULL; i++) {
+		if (strcmp(debug_break_strings[i], type) == 0) {
+			MAGIC_BREAK;
+			return;
+		}
+	}
+}
+#else
+void debug_break(const char *type) {
+	return;
+}
+#endif
+
