4d9b53510f482ab8dfe0ace8b32aff65c207f53a
Justin Scheiner <emdeefive@gmail.com>
2010-11-03 18:43:42 -0400

    Added ls_test.

---
+++ p3/user/progs/ls_test.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <syscall.h>
+#include <string.h>
+#include <simics.h>
+
+#define BUFSIZE 0x500
+char buf[BUFSIZE];
+
+int main(int argc, const char *argv[])
+{
+   int size;
+   char* bufptr = buf;
+   memset(buf, 0xff, BUFSIZE);
+   
+   ls(BUFSIZE, bufptr);
+   
+   while((size = strlen(bufptr)) > 0)
+   {
+      printf("%s\n", bufptr);
+      bufptr += size + 1; 
+   }
+   MAGIC_BREAK;
+
+   return 0;
+}
+
