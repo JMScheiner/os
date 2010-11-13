56104099ba0637e8b199daca117fea65aa37b6cf
Timothy James Wilson <tjwilson@unix10.andrew.cmu.edu>
2010-11-12 19:03:05 -0500

    Remove tcbs from hashtable if address space copying fails

--- p3/kern/syscall/lifecycle.c
+++ p3/kern/syscall/lifecycle.c
@@ -251,6 +251,12 @@ void fork_handler(volatile regstate_t reg)
 
 fork_fail_dup: 
    free_thread_resources(new_tcb);
+
+   mutex_lock(&tcb_table.lock);
+   /* Remove ourself from the global table of threads. */
+   hashtable_remove(&tcb_table, new_tcb->tid);
+   mutex_unlock(&tcb_table.lock);
+   
    new_pcb->thread_count = 0;
 fork_fail_tcb:
 fork_fail_dup_regions: 
