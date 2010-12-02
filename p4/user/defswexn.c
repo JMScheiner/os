ff88088a939e1089cf0ce9246e586ade9d2cdefc
Justin Scheiner <emdeefive@gmail.com>
2010-12-01 20:29:18 -0500

    defswexn restructuring plus a nice pagefault message

--- p4/user/defswexn.c
+++ p4/user/defswexn.c
@@ -22,6 +22,7 @@
 
 #define ESUCCESS 0
 #define EFAIL -1
+#define EFAULT -2
 
 /** @brief The stack that the below code executes on. */
 unsigned char _defswexn_stack_buf[SWEXN_STACKSIZE] = {0};
@@ -45,8 +46,7 @@ int _handle_pagefault(ureg_t* ureg)
    {
       if(new_pages((void*)PAGE_OF(addr), PAGE_SIZE) < 0)
          return EFAIL;
-      else 
-         return ESUCCESS;
+      return ESUCCESS;
    }
    return EFAIL;
 }
@@ -61,31 +61,27 @@ int _handle_pagefault(ureg_t* ureg)
 */
 void _defswexn(void* arg, ureg_t* ureg)
 {
-   int result;
    assert(arg == NULL);
    switch(ureg->cause)
    {
       case IDT_PF: 
-         result = _handle_pagefault(ureg);
+         if(_handle_pagefault(ureg) < 0)
+         {
+            printf("Page fault on 0x%x\n", ureg->cr2);
+            exit(EFAULT);
+         }
          break;
-
-      //TODO Install default handlers for the rest of the exceptions. 
       
+      /* Add other fault handlers as useful here. */
+
       default: 
-         /* FIXME Obnoxiously MAGIC_BREAK if we haven't implemented a handler
-          *  for this particular exception. */
-         MAGIC_BREAK;
-         result = EFAIL;
+         printf("Failed to handle exception %d\n", ureg->cause);
+         exit(EFAULT);
    }
    
    /* Only reregister a handler if we succeeded in fixing the problem. */
-   if(result >= 0)
-      swexn(_defswexn_stack_ptr, _defswexn, 0, ureg);
-
-   // These should go with each failure.
-   printf("Failed to handle exception %d\n", ureg->cause);
-   // FIXME Un magic this magic constant
-   exit(-2);
+   swexn(_defswexn_stack_ptr, _defswexn, 0, ureg);
+   printf("Failed to reregister exception handler.\n");
+   exit(EFAULT);
 }
-
 
